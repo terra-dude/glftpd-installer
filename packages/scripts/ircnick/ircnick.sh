05b1d706db81f7e5931a995c2c5e9ac4eac4a3cd
teqnodude <36924638+teqnodude@users.noreply.github.com>
2019-12-02 23:19:56 +0100

    Update ircnick.sh

--- packages/scripts/ircnick/ircnick.sh
+++ packages/scripts/ircnick/ircnick.sh
@@ -20,7 +20,7 @@ then
 else
         if [ -z "`tac $logfile | grep -i "invite:" | grep -i $1`" ]
         then
-                echo "User has not been invited into chans since date "`cat $logfile | head -n2 | tail -n1 | awk -F '[ :]+' '{print $2}''{print $3}''{print $7}'`
+                echo "User has not been invited into chans since "`cat $logfile | head -n2 | tail -n1 | awk -F '[ :]+' '{print $2}''{print $3}''{print $7}'`
         else
                 tac $logfile | grep -i "invite:" | grep -i $1 | head -1 | awk -F " " '{print $1" "$2" "$3" "$4" "$5" "$6" ircnick: "$7" username: "$8}'
         fi
